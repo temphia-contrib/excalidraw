7f6d79af08ed0ab31324b1b9643c96bcd03278fd
bornjre <bornjre@gmail.com>
2023-02-24 05:33:45 +0545

    add temphia-frontend package

--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { registerPlugFactory } from "temphia-frontend/dist/cjs/engine/register";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+registerPlugFactory("excalidraw.main", (opts) => {
+  const root = ReactDOM.createRoot(opts["target"] || document.body);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+})
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
