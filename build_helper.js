40fbb5998773c3ccc864fc521c6472ee263a7d9b
bornjre <bornjre@gmail.com>
2023-02-25 01:20:02 +0545

    build script

---
+++ build_helper.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+
+fs.readFile('./build/asset-manifest.json', 'utf8', (err, data) => {
+    if (err) {
+        console.log(`Error reading file from disk: ${err}`)
+    } else {
+        const asset = JSON.parse(data)
+        const files = asset["files"]
+
+        const mainjs = files["main.js"]
+        const maincss = files["main.css"]
+
+        fs.readFile("./.temphia/bprint_template.yaml", (e1, d1) => {
+            const tpl = d1.toString('utf8')
+            tpl.replace("main.xxxx.js", mainjs)
+            tpl.replace("main.xxxx.css", maincss)
+            fs.writeFileSync('./.temphia/bprint.yaml', tpl, 'utf8')
+        })
+
+        console.log("@asset", asset["files"])
+
+
+    }
+})
